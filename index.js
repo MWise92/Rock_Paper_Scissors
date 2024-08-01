2bdc76caefa8103cdd617df7018c4bad77f4b8c1
Mitchell Wise <mitch_wize@yahoo.com>
2024-07-31 22:19:42 -0500

    wrote logic to get computer choice

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // console.log("Hello, World")
 function getComputerChoice() {
-    return Math.random("rock" || "paper" || "scissors");
+    if (Math.random()<= 0.34) {
+        return "rock";
+    } else if (Math.random()<= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
-console.log(Math.random);
+console.log(getComputerChoice());
